b44b46e808f46fe04263fcfbd61ca9de9571fe25
captivating-mapping-7374 <mxharris1094@yahoo.com>
2020-02-03 20:19:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,19 +17,9 @@ def line(array)
   end
 end
 
-def take_a_number(array,name)
-    if array.length >= 1
-    new_array = []
-    counter = 1 
-    array.each do |name|
-      new_array.push("#{counter}. #{name}")
-      counter += 1 
-    end 
-    puts "The line is currently: #{new_array.join(" ")}"
-  else
-    puts "The line is currently empty."
-    new_array.push("#{counter}. #{name}")
-  end
+def take_a_number(line, new_person)
+  line << new_person
+  puts "Welcome, #{new_person}. You are number #{line.length} in line."
 end
   
 
